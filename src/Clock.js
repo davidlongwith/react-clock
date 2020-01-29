4f69f1d704565abb630b9acccaae8dd6e3377061
David Longwith <david@davidlongwith.com>
2020-01-28 21:02:16 -0500

    functional clock

---
+++ src/Clock.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Component } from "react";
+import { getClockTime } from "./lib";
+
+export default class Clock extends Component {
+  constructor() {
+    super();
+    this.state = getClockTime();
+  }
+
+  componentDidMount() {
+    console.log("Starting Clock");
+    this.ticking = setInterval(() => this.setState(getClockTime()), 1000);
+  }
+
+  render() {
+    const { hours, minutes, seconds, ampm } = this.state;
+    return (
+      <div className="clock">
+        <span>{hours}</span>
+        <span>:</span>
+        <span>{minutes}</span>
+        <span>:</span>
+        <span>{seconds}</span>
+        <span>{ampm}</span>
+      </div>
+    );
+  }
+}
